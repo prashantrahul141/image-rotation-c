2333fdc8065a525a12d22a61cc99a92b92c326b3
Prashant <prashantrahul141@protonmail.com>
2024-03-22 16:30:16 +0530

    open image, load, call rotate_image, save image.

--- main.c
+++ main.c
@@ -28,6 +28,60 @@ int main(int argc, char *argv[]) {
     return 0;
   }
 
+  // rotationn angle to apply.
+  float rotation_angle = atof(argv[1]);
+  if (rotation_angle == 0.0f) {
+    fprintf(stderr,
+            "Invalid rotation angle, the angle should be in degrees: %s\n%s",
+            argv[1], USAGE);
+    return 1;
+  }
+
+  // convert degrees to radians for internal usage.
+  rotation_angle = rotation_angle / 57.32;
+
+  // filepath
+  char *filepath = argv[2];
+  printf("[INFO] : Loading image : %s\n", filepath);
+
+  // width, height, channels of original image.
+  int image_width, image_height, image_channels;
+
+  // load image.
+  unsigned char *o_image =
+      stbi_load(filepath, &image_width, &image_height, &image_channels, 0);
+
+  // incase image loading fails.
+  if (o_image == NULL) {
+    fprintf(stderr, "[ERROR] : Failed to load image : %s\n", filepath);
+    return 1;
+  }
+
+  printf("[INFO] : Loaded image data\n");
+
+  printf("[INFO] : Image width : %d, image height : %d\n", image_width,
+         image_height);
+
+  // allocate memory for new image.
+  unsigned char *new_image = (unsigned char *)calloc(
+      sizeof(unsigned char) * image_width * image_height * image_channels, 1);
+
+  rotate_image(o_image, image_height, image_width, image_channels, new_image,
+               rotation_angle);
+
+  // writing new image.
+  int result = stbi_write_jpg("output.jpg", image_width, image_height,
+                              image_channels, new_image, 100);
+
+  if (result == 0) {
+    fprintf(stderr, "[ERROR] : Failed to write new image.\n");
+  } else {
+    printf("[INFO] : Wrote new image: output.jpg\n");
+  }
+
+  // freeing image.
+  stbi_image_free(o_image);
+  free(new_image);
 
   return 0;
 }
